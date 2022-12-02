0bb0ffc9166c327150a9ce444b10149ae7b7e45f
MarkRanielPerez <20-06887@g.batstate-u.edu.ph>
2022-12-02 16:01:24 +0800

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PackagesComponent } from './packages/packages.component';
+import { combineLatest } from 'rxjs';
+
+
+const appRoute: Routes = [
+  { 
+    path: 'login', component: AuthComponent
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'packages', component: PackagesComponent
+  }
+]
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    RegisterComponent,
+    NavbarComponent,
+    PackagesComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
